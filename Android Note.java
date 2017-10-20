9ed27a9241dd0e1d1a81a0c33144598080856ef6
LuongHH <luonghh@ominext.com>
2017-10-20 13:54:02 +0700

    init contents

---
+++ Android Note.java
@@ -0,0 +1,14 @@
+Blog
+
+	https://academy.realm.io/
+	https://blog.mindorks.com/tagged/android
+	
+Android Helper
+
+	https://materialdesignicons.com/
+	http://html-color-codes.info/
+	https://github.com/raywenderlich/java-style-guide
+	http://www.jsoneditoronline.org/
+	http://www.jsonschema2pojo.org/
+	https://romannurik.github.io/AndroidAssetStudio/nine-patches.html
+	http://rogerdudler.github.io/git-guide/index.vi.html
