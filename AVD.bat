97cf4e16daf8b199461741f80cb412bf547f9438
LuongHH <luonghh@ominext.com>
2017-11-03 14:18:09 +0700

    add AVD.bat

---
+++ AVD.bat
@@ -0,0 +1,4 @@
+cd C:\Users\\GamePC\AppData\Local\Android\sdk\tools
+emulator -list-avds
+pause
+emulator -avd Samsung_Galaxy_J5_API_23
--- Android Note.java
+++ Android Note.java
@@ -2,6 +2,8 @@ Blog
 
 	https://academy.realm.io/
 	https://blog.mindorks.com/tagged/android
+	https://androidcoban.com/20-open-source-android-tuyet-voi-giup-ban-code-tot-hon-p1.html
+	http://www.devexchanges.info/2017/02/android-recyclerview-dynamically-load.html
 	
 Android Helper
 
@@ -12,3 +14,9 @@ Android Helper
 	http://www.jsonschema2pojo.org/
 	https://romannurik.github.io/AndroidAssetStudio/nine-patches.html
 	http://rogerdudler.github.io/git-guide/index.vi.html
+
+	
+Library
+	
+	com.github.esafirm.android-image-picker
+	https://blog.autsoft.hu/the-android-and-ios-alternative-library-collection/
