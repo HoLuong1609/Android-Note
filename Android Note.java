0aa76b109636881e825c85f3a20e0a4d10658d82
Aloha <holuong1609@gmail.com>
2018-03-20 09:47:05 +0700

    proandroiddev

--- Android Note.java
+++ Android Note.java
@@ -4,6 +4,7 @@ Blog
 	https://blog.mindorks.com/tagged/android
 	https://androidcoban.com/20-open-source-android-tuyet-voi-giup-ban-code-tot-hon-p1.html
 	http://www.devexchanges.info/2017/02/android-recyclerview-dynamically-load.html
+	https://proandroiddev.com/
 	
 Icon
 	https://www.shareicon.net
